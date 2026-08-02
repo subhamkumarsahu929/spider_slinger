19e676a4f51a1cd2c19d1041268525ebefcc58d8
Arijit Sanyal <sanyalarijit@gmail.com>
2026-08-02 09:39:59 +0530

    Added config fixes

--- lib/ui/screens/leaderboard_screen.dart
+++ lib/ui/screens/leaderboard_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 
 class LeaderboardScreen extends StatefulWidget {
   const LeaderboardScreen({super.key});
